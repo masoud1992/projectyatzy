c855f4e53b98a8cc20050a14f7e206c4847f9d47
Unknown <ali@faradji.se>
2017-03-27 15:28:06 +0200

    började på totalScore

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
 
@@ -27,19 +26,34 @@ $(document).ready(function() {
     });
       //listener on keys
     document.onkeypress = function(event) {
-        event = event || window.event;
-          //get which key was pressed
-        key = event.code;
-        // removes excess strings
-        key = key.replace(/\D/g, '');
-        //make key an int
-        key = parseInt(key,10);
-        // if it's a number call endturn()
-        if (key >= 0 && key <= 9) {
-            endTurn();
+      var placeHolder = event.target;
+      console.log(placeHolder.id);
+      //   event = event || window.event;
+      //     //get which key was pressed
+      //  key = event.code;
+       //
+      //   // removes excess strings
+      //   key = key.replace(/\D/g, '');
+      //   //make key an int
+      //   key = parseInt(key,10);
+
+        $(".throwDiceButton").click(function() {
+            var key = $(placeHolder).val();
+            console.log(key);
+            key = parseInt(key,10);
+          // if it's a number call endturn()
+          if (key >= 0 && key <= 9) {
+
+              endTurn();
+
+              totalScore(key);
+            }
+
+        });
+
         }
 
-    };
+    // };
 
     function endTurn() {
       //reset counter
@@ -49,57 +63,12 @@ $(document).ready(function() {
 
 
     }
+    function totalScore(a){
+    var current =  $(this).find(".playerOne").val();
+    current = parseInt((current + a),10);
+    console.log(current);
+    $(".playerOne").html(current);
+    // $(this).find("").val(current);
+    }
 
 });
-$( document ).ready(function() {
-	var a = {
-			1: "&#9856;",
-			2: "&#9857;",
-			3: "&#9858;",
-			4: "&#9859;",
-			5: "&#9860;",
-			6: "&#9861;"
-		}
-	
-	populateBoard();
-    function populateBoard(){		
-		for (var i = 1; i <= 6; i++){		
-		$(".playground").append('<p class="dice-symbol unmarked">' + a[i] + '</p>');
-		}
-	}
-	var throwCounter = 0;
-
-	function throwDice(dicesToThrow) {
-			throwCounter++;
-
-			var dices = [];
-			
-			
-			for(var i = 0; i < dicesToThrow; i++){
-				var randomNum = Math.floor((Math.random() * 6) + 1); 
-				dices.push(randomNum);
-			}
-
-		return dices;
-		
-	}
-
-
-	$(".throwDiceButton").click(function(){
-		var dicesToThrow = $(".unmarked").length;
-		
-		var arrayOfThrownDices = throwDice(dicesToThrow);
-		for (var i = 0; i < arrayOfThrownDices.length; i++){
-			
-		$(".choosenDices").append('<p class="dice-symbol">' + a[i] + '</p>');
-		}
-		
-		
-		
-	});
-
-});
-
-
-
-
