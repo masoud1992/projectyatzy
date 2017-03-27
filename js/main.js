995d34ea7babd1943ce5c31e3a0b43eafbd8db1a
Unknown <ali@faradji.se>
2017-03-27 12:10:24 +0200

    la till endTurn och listenern och indenterade filerna

--- js/main.js
+++ js/main.js
@@ -1,28 +1,47 @@
-$( document ).ready(function() {
-    
-
-	var throwCounter = 0;
-
-	function throwDice(dicesToThrow) {
-			throwCounter++;
-
-			var dices = [];
-			
-			
-			for(var i = 0; i < dicesToThrow; i++){
-				var randomNum = Math.floor((Math.random() * 6) + 1); 
-				dices.push(randomNum);
-			}
-
-		return dices;
-		
-	}
+$(document).ready(function() {
 
 
-	$(".throwDiceButton").click(function(){
-		var dicesToThrow = $(".unmarked").length;
-		throwDice(dicesToThrow);
-		
-	});
+    var throwCounter = 0;
+
+    function throwDice(dicesToThrow) {
+        throwCounter++;
+
+        var dices = [];
+
+
+        for (var i = 0; i < dicesToThrow; i++) {
+            var randomNum = Math.floor((Math.random() * 6) + 1);
+            dices.push(randomNum);
+        }
+
+        return dices;
+
+    }
+
+
+    $(".throwDiceButton").click(function() {
+        var dicesToThrow = $(".unmarked").length;
+        throwDice(dicesToThrow);
+
+    });
+
+    document.onkeypress = function(event) {
+        event = event || window.event;
+
+        key = event.code;
+        key = key.replace(/\D/g, ''); // removes excess strings
+
+        if (key >= 0 && key <= 9) {
+            endTurn();
+        }
+
+    };
+
+    function endTurn() {
+        throwCounter = 0;
+        $(this).find(".playground").empty();
+
+
+    }
 
 });
