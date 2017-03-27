a7489ac83d050825fdac8c26afa512f7b4083910
Unknown <magnusnorlin@gmail.com>
2017-03-27 13:49:10 +0200

    populateBoard() för att få fram initiala tärningar
    global hash-table med utf8-tärningar
    magnus och louise

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
-    
+	var a = {
+			1: "&#9856;",
+			2: "&#9857;",
+			3: "&#9858;",
+			4: "&#9859;",
+			5: "&#9860;",
+			6: "&#9861;"
+		}
+	
+	populateBoard();
+    function populateBoard(){		
+		for (var i = 1; i <= 6; i++){		
+		$(".playground").append('<p class="dice-symbol unmarked">' + a[i] + '</p>');
+		}
+	}
 
 	var throwCounter = 0;
 
@@ -21,8 +35,19 @@ $( document ).ready(function() {
 
 	$(".throwDiceButton").click(function(){
 		var dicesToThrow = $(".unmarked").length;
-		throwDice(dicesToThrow);
+		
+		var arrayOfThrownDices = throwDice(dicesToThrow);
+		for (var i = 0; i < arrayOfThrownDices.length; i++){
+			
+		$(".choosenDices").append('<p class="dice-symbol">' + a[i] + '</p>');
+		}
+		
+		
 		
 	});
 
 });
+
+
+
+
