256649a9dbd0051d1cd37fc76123487581d49c16
Masoud <masoud.h@live.se>
2017-04-11 13:03:41 +0200

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# projectyatzy
-yatzeee
--- db-dumps/yatzyDb.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `yatzy`.`score` (
-  `userName` VARCHAR(45) NOT NULL,
-  `totalScore` INT NULL,
-  `wonGames` INT NULL,
-  PRIMARY KEY (`userName`));
